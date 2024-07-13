fd33ec0eac7f508126fd57b3499daf7709e5688c
Dmitry <glavandimoncik@gmail.com>
2024-07-13 12:38:07 +0300

    new code

--- menu/src/menu.js
+++ menu/src/menu.js
@@ -4,17 +4,16 @@ import './Style.css';
 
 const Menu = () => {
     const menuItems = CodeMenu.menu;
-    return (
-        <div className='menu-container'>
-            <ul className='menu'>
-                {menuItems.map((el) => (
-                    <li key={el.menuPos}>
-                        <a href='#'>{el.menuContent}</a>
+        return(
+            <div className='menu-container'>
+                <ul className='menu'>
+                    {menuItems.map((el) => (
+                        <li key={el}>
+                            <a href='#'>{el.menuContent}</a>
                         </li>
-                ))}
-            </ul>
-        </div>
-    )
+                    ))}
+                </ul>
+            </div>
+        )
 }
-
 export default Menu;
